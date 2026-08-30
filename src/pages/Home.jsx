7465fc0448f30674215f065714b8c04ccf3d696f
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-31 00:54:14 +0600

    Fetch and display products on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+
+
     return (
-        <div>
-            <p>Home</p>
+        // all cards
+        <div style={{display:"grid", gridTemplateColumns:"repeat(4, 1fr)", gap:"20px"}}>
+            {products.map(product =>(
+                <div style={{border: "2px solid gray"}}>
+                    <img style={{width: "100%"}} src={product.image} alt="" />
+                    <p>{product.title}</p>
+                    <p>{product.price}</p>
+                </div>
+            ))}
         </div>
     );
 };
