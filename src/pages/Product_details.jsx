c60ce1fa8fd73c9d64a25b30f5108a0ef4581d01
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-31 02:37:54 +0600

    feat: update product details routing and display functionality

--- src/pages/Product_details.jsx
+++ src/pages/Product_details.jsx
@@ -1,7 +1,16 @@
+import { useParams } from "react-router";
+import useFetch from "../hooks/useFetch";
+
 const Product_details = () => {
+
+    const {products} = useFetch();
+    const {OurId} = useParams();
+    const singleProduct = products.find(p => p.id == OurId) 
+
     return (
         <div>
             <p>Products Details Page</p>
+            <img src={singleProduct?.image} alt="" />
         </div>
     );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -26,7 +26,7 @@ export const router = createBrowserRouter([
                 element: <Products/>
             },
             {
-                path: 'products/:id',
+                path: 'products/:OurId',
                 element: <Product_details/>
             }
         ]
